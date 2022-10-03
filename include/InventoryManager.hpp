3902ab222ea9841334da3c220ff6c7fb0fd5accd
Joseph Wehby <josephw31@hotmail.com>
2022-10-03 15:14:28 -0400

    #8 removed random code that was inserted in somehow

--- include/InventoryManager.hpp
+++ include/InventoryManager.hpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef INVENTORY_MANAGER_HPP
 #define INVENTORY_MANAGER_HPP
 
@@ -16,22 +15,3 @@ private:
 };
 
 #endif
-=======
-#ifndef INVENTORY_MANAGER_HPP
-#define INVENTORY_MANAGER_HPP
-
-#include "ActiveInventory.cpp"
-
-class InventoryManager
-{
-	public:
-		inventoryManager(bool);
-		~inventoryManager();
-		int userInput();
-	private:
-		bool command_line;
-		std::unique_ptr<ActiveInventory> active_inventory(new ActiveInventory());
-};
-
-#endif
->>>>>>> 646baff9e088b4c0dd7310412708090615e93b60
--- src/InventoryManager.cpp
+++ src/InventoryManager.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <string>
 #include "InventoryManager.hpp"
@@ -71,75 +70,4 @@ int InventoryManager::userInput()
 
     return 0;
 }
-=======
-#include <iostream>
-#include <string>
-//#include "InventoryManager.hpp"
 
-InventoryManager::InventoryManager(bool cli)
-{
-	command_line = cli;
-}
-
-InventoryManager::~InventoryManager()
-{
-	/* using smart pointer for active inventory so no deletion neccessary */
-}
-
-/* gets command line input from user and calls methods from active inventory class */
-int InventoryManager::userInput()
-{
-	std::string argument, name, category, value; 
-	
-	if (command_line == false) {
-        std::cout << "Command line is currently set to false" << std::endl;
-        return -1;
-    }
-
-	std::cout << "Add, Remove, or Update item: ";
-	std::cin >> argument;	
-
-	/* switch on argument specified from user and then prompt them accordingly for further input */
-	switch(argument)
-	{
-		case "Add":
-            std::cout << "Perishable/Nonperishable Name: ";
-			std::cin >> category >> name;
-
-			if (active_inventory->addItem(name, category) != -1) {
-				std::cout << "\nAdded " << name << " of type " << category << std::endl;
-			} else {
-                return -1;
-            }
-			break;
-
-		case "Remove":
-			std::cout << "\nName: ";
-			std::cin >> name;
-
-			if (active_inventory->removeItem(name) != -1) {
-				std::cout << "\nRemoved " << name << std::endl;
-			} else {
-                return -1;
-            }
-			break;
-
-		case "Update":
-			std::cout << "\nName Field Value: ";
-			std::cin >> name >> category >> value;
-			
-			if (active_inventory->updateItem(name, category, value) != -1) {
-				std::cout << "\nUpdated " << category << " of " << name << " to " << value << std::endl;
-			} else {
-                return -1;
-            }
-			break;
-
-		default:
-			std::cout << "Usage: <Add | Remove | Update>" << std::endl;
-			break;
-	}
-
-    return 0;
-}
->>>>>>> 646baff9e088b4c0dd7310412708090615e93b60
