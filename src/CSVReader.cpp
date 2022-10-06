2a844a4f7ee5823a79f845f0148a8396347a584b
jhulen <clear>
2022-10-05 22:03:57 -0400

    gh-1 practicing routinely committing, just added a few lines to CSVReader

--- src/CSVReader.cpp
+++ src/CSVReader.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 #include <fstream>
 
-#include "CSVReader.hpp"
+//#include "CSVReader.hpp"
 
 int main(void)
 {
 
-	std::string name;
+	std::string name, id, category, sub_cat, quant, sale_price;
+	std::string tax, total_price, buy_cost, profit, expiry; 
 
 	std::ifstream csv_file("CSVInput/big_file.csv");
 
@@ -18,8 +19,29 @@ int main(void)
 	while(csv_file.good()){
 
 		getline(csv_file, name, ',');
+		getline(csv_file, id, ',');
+		getline(csv_file, category, ',');
+		getline(csv_file, sub_cat, ',');
+		getline(csv_file, quant, ',');
+		getline(csv_file, sale_price, ',');
+		getline(csv_file, tax, ',');
+		getline(csv_file, total_price, ',');
+		getline(csv_file, buy_cost, ',');
+		getline(csv_file, profit, ',');
+		getline(csv_file, expiry, '\n');
 		
-		std::cout << "file name: " << name << '\n';
+		std::cout << "name " << name << '\n';
+		std::cout << "id " << id << '\n';
+		std::cout << "cat " <<category << '\n';
+		std::cout << sub_cat << '\n';
+		std::cout << quant << '\n';
+		std::cout << sale_price << '\n';
+		std::cout << tax << '\n';
+		std::cout << total_price << '\n';
+		std::cout << buy_cost << '\n';
+		std::cout << profit << '\n';
+		std::cout << expiry << '\n';
+
 	}
 
 return 0;
