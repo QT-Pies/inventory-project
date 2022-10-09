509da246a4d1daa03110c8b6e89bfc76c3357967
Joseph Wehby <josephw31@hotmail.com>
2022-10-09 13:14:03 -0500

    main error fixed

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,10 @@ int main(int argc, char**argv){
 	std::string arg, csv_file;
 	bool command_line = false;
 
-	if (argc == 3) {
+	if (argc == 2) {
 		arg = argv[1];
 		csv_file = argv[2];
 		if (arg == "1") command_line = true;
-		file = argv[2];
 	} else {
 		fprintf(stderr, "Usage: bin/main command_line csv_file\n");
 		return -1;
@@ -18,10 +17,10 @@ int main(int argc, char**argv){
 
 	InventoryManager im(command_line);
 	im.readCSVFile(csv_file);
+	
 	while (true) {
-		if(im.userInput() == -1) break;
+		if(im.userInput() == -1) return -1;
 	}
-
 
 	im.fileOutput();
 	return 0;
