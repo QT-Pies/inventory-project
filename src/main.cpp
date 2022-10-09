86163e69f8207c233477729e4763a4676c287e11
Joseph Wehby <josephw31@hotmail.com>
2022-10-09 13:14:03 -0500

    #2 main error

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,22 @@
 
 int main(int argc, char**argv){
 
-	std::string arg, file;
+	std::string arg;
 	bool command_line = false;
 
-	if (argc == 3) {
+	if (argc == 2) {
 		arg = argv[1];
 		if (arg == "1") command_line = true;
-		file = argv[2];
 	} else {
-		fprintf(stderr, "Usage: bin/main command_line file_name\n");
+		fprintf(stderr, "Usage: bin/main command_line\n");
 		return -1;
 	}
 
-	InventoryManager im(command_line, file);
+	InventoryManager im(command_line);
 	
 	while (true) {
-		if(im.userInput() == -1) break;
+		if(im.userInput() == -1) return -1;
 	}
 
-	im.fileOutput();
 	return 0;
 }
