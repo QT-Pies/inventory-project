37844d3939f8635ddb7f5f0b9ebfbda835cb1318
Noah Shoap <nshoap@vols.utk.edu>
2022-10-27 10:44:56 -0400

    #23 testing on another file

--- src/SalesGeneratorDriver.cpp
+++ src/SalesGeneratorDriver.cpp
@@ -1,9 +1,26 @@
 #include "SalesGenerator.hpp"
 
-int main() {
-    SalesGenerator generator("CSVInput/backorder_test.csv", "12/28/1999");
+static void printUsage(std::ostream &out) {
+    out << "usage: ./gen-inventory output_file.csv #undefined #non-perishable "
+           "#perishable --flags"
+        << std::endl;
 
-    generator.generateTransactions(10);
+    out << "usage: ./gen-sales inventory.csv MM/DD/YYYY #of transactions" << std::endl;
+}
+
+int main(int argc, char** argv) {
+
+    /* Print usage if not correct amount of args. */
+    if (argc != 4) {
+        printUsage(std::cerr);
+        return 1;
+    }
+
+    /* Otherwise, run the generator. */
+
+    SalesGenerator generator(argv[1], argv[2]);
+
+    generator.generateTransactions(std::stoul(argv[3]));
 
     return 0;
 }
