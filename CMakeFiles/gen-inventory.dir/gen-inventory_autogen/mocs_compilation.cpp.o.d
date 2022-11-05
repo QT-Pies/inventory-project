20fdd2fb59fe2bfe825df5fb882367a56c803e30
vbroda17 <vbroda@vols.utk.edu>
2022-11-05 10:48:37 -0400

    #63 merged with 90

--- CMakeFiles/gen-inventory.dir/gen-inventory_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/gen-inventory.dir/gen-inventory_autogen/mocs_compilation.cpp.o: \
- /home/vbroda/inventory-project/gen-inventory_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- CMakeFiles/gen-sales_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gen-sales_autogen"
-  "gen-sales_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gen-sales_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
