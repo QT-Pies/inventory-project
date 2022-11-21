90fb9b1122ac51804c75a18ee8bca9913a6f3fae
vbroda17 <vbroda@vols.utk.edu>
2022-11-20 22:00:18 -0500

    #128 ran cmake --build . --target clean

--- CMakeFiles/gen-sales.dir/gen-sales_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/gen-sales.dir/gen-sales_autogen/mocs_compilation.cpp.o: \
- /home/vbroda/inventory-project/gen-sales_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- CMakeFiles/unit_tests.dir/unit_tests_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/unit_tests.dir/unit_tests_autogen/mocs_compilation.cpp.o: \
- /home/vbroda/inventory-project/unit_tests_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
